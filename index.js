925dace77c970618c597907110d8685bbce7cafc
Marvin <124067182+babi-nos@users.noreply.github.com>
2023-05-09 15:34:10 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -238,20 +238,229 @@
 // SI L'éLEMNT N4EST PAS PRENSENT IL RENVERA -1
 
 /**************************************************************************************************************************/
-// FUNCTION EN UTILISANT LES PARAMETRE
+// FUNCTION EN UTILISANT LES PARAMETRE POUR SAVOIR SI LA LETTRE EXCISTE
 
-const str = "EJ suis un padawan !!";
+// const str = "EJ suis un padawan !!";
 
-function isHear(str, lettre) {
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === lettre) {
-      return console.log(`Oui il y a un ${lettre}`);
-    }
-  }
-  return console.log(`Non il n'y a pas se ${lettre}`);
-}
+// function isHear(str, lettre) {
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === lettre) {
+//       return console.log(`Oui il y a un ${lettre}`);
+//     }
+//   }
+//   return console.log(`Non il n'y a pas se ${lettre}`);
+// }
 
-isHear(str, 's');
+// isHear(str, "s");
+
+/**************************************************************************************************************************/
+// TROUVER QUEL SALARIES A UN SALAIRE DE PLUS DE 50K
+
+// const salaries = [
+//   {
+//     name: "jhon",
+//     salaire: 2000
+//   },
+//   {
+//     name: "emma",
+//     salaire: 100000
+//   },
+//   {
+//     name: "tarik",
+//     salaire: 23000
+//   },
+// ];
+
+// console.log(salaries.find(salaries => salaries.salaire > 50000));
+
+/**************************************************************************************************************************/
+// tableau.includes(element); // true / false
+// tableau.find((element) => condition); // element / undefined
+// tableau.indexOf(element); // index / -1
+// tableau.findIndex((element) => condition); // index / -1
+/**************************************************************************************************************************/
+
+// UTILISER FILTER() POUR TROUVER ET METTRE DANS DES VARIABLE LES POSITIVE ET NEGATIVE DE MON TABLEAUX NUMBER
+
+// const number = [1, -5, 45, -25, 964, -3, 54, 788];
+
+// const negative = number.filter(nombre => nombre < 0)
+// const positive = number.filter(nombre => nombre > 0)
+
+// console.log(negative);
+// console.log(positive);
+
+/**************************************************************************************************************************/
+// FILTER QUE LE NOMBRE PAIRE ET IMPAIRE
+
+// const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const paire = number.filter((numbre) => (numbre % 2 == 0));
+
+// console.log(paire);
+// console.log(number);
+
+/**************************************************************************************************************************/
+// FILTRER TOUT LES PERSONNE AVEC UN SALAIRE DE +- DE 50K
+
+// const salariés = [
+//   {
+//     name: "boby",
+//     salaire: 25000,
+//   },
+//   {
+//     name: "charle",
+//     salaire: 35000,
+//   },
+//   {
+//     name: "goerge",
+//     salaire: 100000,
+//   },
+//   {
+//     name: "sarah",
+//     salaire: 55000,
+//   },
+// ];
+
+// const filtr = salariés.filter((personne) => personne.salaire > 50000);
+
+// console.log(filtr);
+
+/**************************************************************************************************************************/
+// BOUCLE MAPS POUR RENVOIYER DANS CARRE TOUT LES NOMBRE DE NUMBER AU CARRER
+
+// const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const carres = number.map((index) => index * index);
+
+// console.log(carres);
+
+/**************************************************************************************************************************/
+//EXCO
+// const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const carres = number.map((nombre, index) => {
+//   if (index % 2 == 1) return 0;
+
+//   return nombre * nombre
+// });
+
+// console.log(carres);
+
+/**************************************************************************************************************************/
+// ONT UTILISE LA BOUCLE Map()
+
+// const data = [
+//   {
+//     nom: "john",
+//     salaire: 25500,
+//   },
+//   {
+//     nom: "amel",
+//     salaire: 5500,
+//   },
+//   {
+//     nom: "richard",
+//     salaire: 55000,
+//   },
+//   {
+//     nom: "amed",
+//     salaire: 15000,
+//   },
+//   {
+//     nom: "sylvie",
+//     salaire: 355000,
+//   },
+//   {
+//     nom: "patoche",
+//     salaire: 255000,
+//   },
+// ];
+
+// const listSaliries = data.map(personne => personne.nom );
+
+// console.log(listSaliries);
+
+/**************************************************************************************************************************/
+// CALCULER UNE MOYENNE AVEC REDUCE
+
+// const numbr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11];
+
+// // let resultOfTab = 0;
+// // for (let chiffre of numbr) {
+// //   resultOfTab += chiffre;
+// // }
+// // console.log(resultOfTab);
+
+// const moyenne = numbr.reduce((acc, number) => acc + number) / numbr.length;
+
+// console.log(moyenne.toFixed(1));
+
+/**************************************************************************************************************************/
+//AUTRE POSIBILITER DE DEFINIR UNE CONST DIRECTEMENT DANS REDUCE AVEC LE CALCUL
+// const numbr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11];
+
+// const add = (a, b) => a * b;
+// const total = numbr.reduce(add, 100);
+
+// console.log(total);
+
+// const str = "je suis un padaaawannn !";
+
+// // const mots = str.split(" ");
+
+// // console.log(mots.reduce((acc, mot) => acc + " " + mot));
+// console.log([...str].reduce((acc, mot) => acc + 1, 0) );
 
 
 /**************************************************************************************************************************/
+// UTILISER REDUCE POUR FAIRE LA MOYENNE
+
+
+// const ecoleNote = [
+//   {
+//     matier: "sport",
+//     note: 20,
+//   },
+//   {
+//     matier: "angalis",
+//     note: 14,
+//   },
+//   {
+//     matier: "francais",
+//     note: 6,
+//   },
+//   {
+//     matier: "histoire",
+//     note: 12,
+//   },
+//   {
+//     matier: "geo",
+//     note: 14,
+//   },
+//   {
+//     matier: "math",
+//     note: 5,
+//   },
+//   {
+//     matier: "physique",
+//     note: 7,
+//   },
+//   {
+//     matier: "chimie",
+//     note: 10,
+//   },
+//   {
+//     matier: "espagnol",
+//     note: 8,
+//   },
+//   {
+//     matier: "techno",
+//     note: 12,
+//   },
+// ];
+
+// let moy = ecoleNote.reduce((acc, current) => acc + current.note, 0) / ecoleNote.length;
+
+
+// console.log(moy);
